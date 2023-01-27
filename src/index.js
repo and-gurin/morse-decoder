a932c04e88f7f0078e4ffe6316314a76ac54345f
and-gurin <and.gurin@gmail.com>
2023-01-27 12:34:48 +0100

    feat: done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let phrase = '';
+    for (let i = 0; i < expr.length/10; i++) {
+        let str = expr.slice(i * 10, (i * 10) + 10);
+        if (str === '**********') {
+            phrase += ' '
+        } else {
+            str = str.slice(str.indexOf('1'));
+            let letter = '';
+            for (let j = 0; j < str.length / 2; j++) {
+                let substr = str.slice(j * 2, (j * 2) + 2);
+                if (substr === '11'){
+                    letter += '-'
+                }
+                else {letter += '.'}   
+            }
+            phrase += MORSE_TABLE[letter];
+        }
+    }
+    return phrase;
 }
 
 module.exports = {
